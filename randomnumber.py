8c00ac9ce56e2d69dd933e4f65d1fa962709421f
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-04-07 20:38:46 -0400

    Update randomnumber.py

--- randomnumber.py
+++ randomnumber.py
@@ -1,11 +1,20 @@
+# Created by: Aden Rao
+# Created on: April 7, 2019
+# This program randomly picks a number between 1 and 6 and then it lets the user enter a number and then it will let you know if it is right.
+
+# Imports math
 import math
 
+# Input for the user to enter a number and it tells them to enter a number
 myGuess = int(input("Type a number from 1 to 6: "))
 
+# Imports random function
 import random
 
+# Value of the correct number
 value = random.randint(1, 6)
 
+# If statement for if the user's answer is equal to the number to guess
 if value == myGuess:
 	print ("Correct!")
 	print (" ")
